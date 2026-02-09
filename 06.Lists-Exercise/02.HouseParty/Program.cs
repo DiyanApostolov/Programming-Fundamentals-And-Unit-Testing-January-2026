7b03f5ebaea7f14e3281c3eeaac1a8310a637be4
Diyan Apostolov <6802398+DiyanApostolov@users.noreply.github.com>
2026-02-09 21:39:26 +0200

    Add solutions for Lists - Exercise

---
+++ 06.Lists-Exercise/02.HouseParty/Program.cs
@@ -0,0 +1,40 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+List<string> guestList = new List<string>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] tokens = Console.ReadLine().Split(" ");
+
+    string name = tokens[0];
+
+    if (tokens[2] == "going!")  //"{name} is going!"
+    {
+        if (guestList.Contains(name))
+        {
+            Console.WriteLine($"{name} is already in the list!");
+        }
+        else
+        {
+            guestList.Add(name);
+        }
+    }
+    else if (tokens[2] == "not") // "{name} is not going!"
+    {
+        if (guestList.Contains(name))
+        {
+            guestList.Remove(name);
+        }
+        else
+        {
+            Console.WriteLine($"{name} is not in the list!");
+        }
+    }
+}
+
+// Output
+foreach (var name in guestList)
+{
+    Console.WriteLine(name);
+}
