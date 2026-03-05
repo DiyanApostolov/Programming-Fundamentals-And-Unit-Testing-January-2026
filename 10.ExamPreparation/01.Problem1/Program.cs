abcb94384fb6f9de569ab8f7e231fe1bf867a69e
Diyan Apostolov <6802398+DiyanApostolov@users.noreply.github.com>
2026-03-05 16:54:13 +0200

    Add solutions

---
+++ 10.ExamPreparation/01.Problem1/Program.cs
@@ -0,0 +1,48 @@
+﻿
+//int number = int.Parse(Console.ReadLine());
+
+//int sumFactorial = 0;
+
+////4532
+//while (number > 0)
+//{
+//    int lastDigit = number % 10;
+
+//    if (lastDigit % 2 == 0)
+//    {
+//        int factorial = 1;
+
+//        for (int i = 1; i <= lastDigit; i++)
+//        {
+//            factorial = factorial * i;
+//        }
+
+//        sumFactorial += factorial;
+//    }
+
+//    number /= 10;
+//}
+
+//Console.WriteLine(sumFactorial);
+
+string number = Console.ReadLine();
+int sumFactorial = 0;
+//4532
+for (int i = 0; i < number.Length; i++)
+{
+    int currentDigit = int.Parse(number[i].ToString());
+
+    if (currentDigit % 2 == 0)
+    {
+        int factorial = 1;
+
+        for (int j = 1; j <= currentDigit; j++)
+        {
+            factorial = factorial * j;
+        }
+
+        sumFactorial += factorial;
+    }
+}
+
+Console.WriteLine(sumFactorial);
---
+++ 10.ExamPreparation/02.Problem2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int[] numbers = Console.ReadLine()
+                        .Split(" ")
+                        .Select(int.Parse)
+                        .ToArray();
+
+int rightMiddleIndex = numbers.Length / 2;
+int leftMiddleIndex = rightMiddleIndex - 1;
+
+int rightMiddleNumber = numbers[rightMiddleIndex];
+int leftMiddleNumber = numbers[leftMiddleIndex];
+
+double averageValue = (rightMiddleNumber + leftMiddleNumber) / 2.0;
+
+Console.WriteLine($"{averageValue:F2}");
